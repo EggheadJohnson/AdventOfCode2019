c8ca33ec8fac878fef639b462a398c2357836c15
Paul Johnson <PaulE.Johnson@gmail.com>
2019-11-30 22:18:51 -0700

    day 01 in the bag

--- solutions/01/a.py
+++ solutions/01/a.py
@@ -5,3 +5,12 @@ file_path = os.path.dirname(os.path.realpath(__file__))
 input_path = file_path + "/a.in.txt"
 
 inpt = open(input_path, 'r')
+
+tot = 0
+
+for i in inpt:
+    tot += int(i)/3 - 2
+
+print tot, tot/3
+
+print tot/3 - 2
--- solutions/01/b.py
+++ solutions/01/b.py
@@ -5,3 +5,17 @@ file_path = os.path.dirname(os.path.realpath(__file__))
 input_path = file_path + "/b.in.txt"
 
 inpt = open(input_path, 'r')
+
+def getFuelReqs(mass):
+    if mass <= 0:
+        return 0
+    return mass + getFuelReqs(mass/3 - 2)
+
+
+tot = 0
+fuel = 0
+
+for i in inpt:
+    fuel += getFuelReqs(int(i) / 3 - 2)
+
+print fuel
